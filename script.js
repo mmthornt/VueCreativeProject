83af70f94c012bd8a547fcfe5b221ee12aa2a672
Molly Thornton <mmthornto@gmail.com>
2019-10-23 21:15:05 +0000

    simplify

--- script.js
+++ script.js
@@ -6,14 +6,10 @@ let app = new Vue({
    },
    methods: {
       async currentHours() {
-         try {
-            this.loading = true;
+         
             const response = await axios.get('https://libraryhours8.lib.byu.edu/api/hours.json');
 
-         }
-         catch (error) {
-            console.log(error)
-         }
+         
       }
    }
 });
