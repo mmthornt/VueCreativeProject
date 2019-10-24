8e85d88e89c547bbb01ab232589f14ef745da0d9
Molly Thornton <mmthornto@gmail.com>
2019-10-24 18:48:07 +0000

    new api

--- script.js
+++ script.js
@@ -2,14 +2,25 @@
 let app = new Vue({
    el: '#app',
    data: {
-
+      current: null,
    },
    methods: {
       async currentHours() {
-         
-            const response = await axios.get('https://libraryhours8.lib.byu.edu/api/hours.json');
+         try {
+            var url = "https://madlibz.herokuapp.com/api/random?minlength=5&maxlength=25";
+            let response = await axios.get(url);
 
-         
-      }
+            
+            console.log(response);
+            this.current = response.data;
+         }
+         catch (error) {
+            console.log(error);
+         }
+
+      },
+   },
+   created() {
+      this.currentHours();  
    }
 });
